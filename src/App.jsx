a752ffb11551fd0934aca9fe7565dbd78673e5d1
Pedro <pedro795027@gmail.com>
2024-04-29 12:17:21 -0300

    feat: configuração das rotas

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App(){
+    return(
+        <div className="App">
+            <h1>React App</h1>
+        </div>
+    )
+}
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import GlobalStyles from './global/styles/globalStyles';
 import reportWebVitals from './reportWebVitals';
-import Home from './pages/Home'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './pages/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/Home",
+    element: <Home/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles/>
-    <Home/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
